3ad22c8498e9ae1d8bfa5908ece46bf8ecf8abdf
VasiliyTemniy <flash_er@mail.ru>
2022-11-06 11:34:05 +0300

    10.3 - ready!

---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,20 @@
+import { Text, View } from 'react-native';
+
+const RepositoryItem = ({ fullName, description, language,
+  forksCount, stargazersCount, ratingAverage, reviewCount }) => {
+
+    return (
+      <View style={{ backgroundColor: 'white' }}>
+        <Text>Full name: {fullName}</Text>
+        <Text>Description: {description}</Text>
+        <Text>Language: {language}</Text>
+        <Text>Stars: {stargazersCount}</Text>
+        <Text>Forks: {forksCount}</Text>
+        <Text>Reviews: {reviewCount}</Text>
+        <Text>Rating: {ratingAverage}</Text>
+      </View>
+    );
+
+};
+
+export default RepositoryItem;
