c48dccde1f7296297ad197e3bd92294de1ad5248
VasiliyTemniy <flash_er@mail.ru>
2022-11-07 20:31:59 +0300

    just add a little error and loading handling

--- rate-repository-app/src/components/RepositoryList/index.jsx
+++ rate-repository-app/src/components/RepositoryList/index.jsx
@@ -1,18 +1,46 @@
-import useRepositories from '../hooks/useRepositories';
-import { FlatList, View, StyleSheet } from 'react-native';
+import useRepositories from '../../hooks/useRepositories';
+import { FlatList, View, StyleSheet, ActivityIndicator } from 'react-native';
+
 import RepositoryItem from './RepositoryItem';
+import Text from '../Text';
+
+import theme from '../../theme';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  activityIndicator: {
+    alignSelf: 'center',
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
 
-  const { repositories } = useRepositories();
+  const { repositories, error, loading } = useRepositories();
+
+  if (loading) return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color={theme.colors.primary} style={styles.activityIndicator}/>
+    </View>
+  );
+
+  if (error) {
+    console.log('error: ', error);
+    return (
+      <View style={styles.container}>
+        <Text>
+          Please, reload the app.
+        </Text>
+      </View>
+    );
+  }
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
