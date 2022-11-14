be208ae89d8188649e8856c13b833e12d11f7a7c
VasiliyTemniy <flash_er@mail.ru>
2022-11-14 20:11:19 +0300

    fix conditional return

--- rate-repository-app/src/hooks/useMeInfo.js
+++ rate-repository-app/src/hooks/useMeInfo.js
@@ -5,9 +5,7 @@ const useMeInfo = () => {
   
   const { data, error, loading, refetch } = useQuery(GET_CURRENT_USER, {fetchPolicy: 'cache-and-network'});
 
-  if (loading) return { loading };
-  if (error) return { error };
-  return { me: data.me, loading, error, refetch };
+  return { me: data?.me, loading, error, refetch };
 };
 
 export default useMeInfo;
